7ad864608d22411fd033c740dbb7bb458e6a9741
Michael Odell <michael@theodells.us>
2023-12-18 16:33:15 -0700

    Use cached-source, and only let brew adjust path once.

--- zsh-homebrew.plugin.zsh
+++ zsh-homebrew.plugin.zsh
@@ -11,15 +11,16 @@ load_brew () {
         return
     fi
 
-    # This is the way brew officially seems to expect you to set up its
-    # variables, although I don't like that it puts itself in front of
-    # standard system paths.
-    #
-    # ref: https://docs.brew.sh/Manpage#shellenv
-    if (( $+functions[znap] )) ; then
-        znap eval homebrew "${brew_path}/bin/brew shellenv"
-    else
-        eval "$(${brew_path}/bin/brew shellenv)"
+    # The brew shellenv code doesn't play nicely with subshells (for instance, in tmux), it may add
+    # itself to the path a second time in a new spot.  But it always sets HOMEBREW_PREFIX, so if that's
+    # unset I know I haven't called it yet.
+    if [[ -z "${HOMEBREW_PREFIX:-}" ]] ; then
+
+        # This (minus the cached-source) is the way brew officially seems to expect you to set up its
+        # variables, although I don't like that it puts itself in front of standard system paths.
+        #
+        # ref: https://docs.brew.sh/Manpage#shellenv
+        cached-source ${brew_path}/bin/brew shellenv
     fi
 
     # Add shell completion for brew and brew-installed tools
