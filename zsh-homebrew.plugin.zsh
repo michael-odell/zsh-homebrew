f4872fced3433db3b403d45afbd8889aacc249fe
Michael Odell <michael@theodells.us>
2024-02-09 17:31:39 -0700

    Login/non-login shell paths, and google-cloud.
    This is a somewhat improved fix to keep PATH consistent across login and
    non-login shells on macos.
    
    I also added some google-cloud-specific paths, should homebrew have
    installed related tools.

--- zsh-homebrew.plugin.zsh
+++ zsh-homebrew.plugin.zsh
@@ -11,21 +11,22 @@ load_brew () {
         return
     fi
 
-    # ~call the hombrew code for login shells (such as tmux shells), because in those cases /etc/profile
-    # on the mac has removed brew's starting path position.
-    if [[ -o login ]] ; then
-
-        # This (minus the cached-source) is the way brew officially seems to expect you to set up its
-        # variables, although I don't like that it puts itself in front of standard system paths.
-        #
-        # ref: https://docs.brew.sh/Manpage#shellenv
-        cached-source ${brew_path}/bin/brew shellenv
-    fi
+    # This (minus the cached-source) is the way brew officially seems to expect you to set up its
+    # variables, although I don't like that it puts itself in front of standard system paths.
+    #
+    # ref: https://docs.brew.sh/Manpage#shellenv
+    cached-source ${brew_path}/bin/brew shellenv
 
     # Add shell completion for brew and brew-installed tools
     #
     # ref: https://docs.brew.sh/Shell-Completion
     fpath+=(${brew_path}/share/zsh/site-functions)
+
+    if [[ -d ${brew_path}/share/google-cloud-sdk ]] ; then
+        source ${brew_path}/share/google-cloud-sdk/path.zsh.inc
+        source ${brew_path}/share/google-cloud-sdk/completion.zsh.inc
+    fi
+
 }
 
 load_brew
